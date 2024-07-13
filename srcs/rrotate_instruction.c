a68e1cdf7eec47211095bb8377a94a6462fec3aa
Senku <tolu_48@hotmail.fr>
2024-07-13 20:43:07 +0200

    Fix rrotate instruction

--- srcs/rrotate_instruction.c
+++ srcs/rrotate_instruction.c
@@ -6,34 +6,51 @@
 /*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 13:09:04 by tkara2            #+#    #+#             */
-/*   Updated: 2024/07/12 13:23:33 by tkara2           ###   ########.fr       */
+/*   Updated: 2024/07/13 20:42:35 by tkara2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+static t_stack *get_prelast_node(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next->next)
+		stack = stack->next;
+	return (stack);
+}
+
 void	rra(t_stack **stack_a)
 {
+	t_stack	*prelast;
 	t_stack	*last;
 
 	if (!stack_a || !*stack_a)
 		return ;
-	last = get_last_node(stack_a);
+	prelast = get_prelast_node(*stack_a);
+	last = get_last_node(*stack_a);
 	last->next = *stack_a;
-	(*stack_a) = last;
+	prelast->next = NULL;
+	*stack_a = last;
 	ft_putendl_fd("rra", STDOUT_FILENO);
 }
+
 void	rrb(t_stack **stack_b)
 {
+	t_stack	*prelast;
 	t_stack	*last;
 
 	if (!stack_b || !*stack_b)
 		return ;
-	last = get_last_node(stack_b);
+	prelast = get_prelast_node(*stack_b);
+	last = get_last_node(*stack_b);
 	last->next = *stack_b;
-	(*stack_b) = last;
+	prelast->next = NULL;
+	*stack_b = last;
 	ft_putendl_fd("rrb", STDOUT_FILENO);
 }
+
 void	rrr(t_stack **stack_a, t_stack **stack_b)
 {
 	rra(stack_a);
